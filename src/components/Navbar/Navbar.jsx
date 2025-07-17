a8518d49e23a396a2719878545279964644d3495
GustavoFSProg <gustavo.prog40@gmail.com>
2025-07-17 12:00:31 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -108,7 +108,7 @@ const LinksContainer = styled.div`
     display: flex;
 
     margin-top: 290px;
-    margin-left: -289px;
+    margin-left: -291px;
     width: auto;
     /* background: green; */
 
