d6bc9f716a75a8445ff4f873441e36e0c7434acd
GustavoFSProg <gustavo.prog40@gmail.com>
2025-07-17 11:44:43 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const Logo = styled.div`
 
   @media screen and (max-width: 800px) {
     margin-top: 35px;
-    margin-left: 12px;
+    margin-left: 2px;
   }
 `;
 
@@ -108,7 +108,7 @@ const LinksContainer = styled.div`
     display: flex;
 
     margin-top: 290px;
-    margin-left: -280px;
+    margin-left: -300px;
     width: auto;
     /* background: green; */
 
